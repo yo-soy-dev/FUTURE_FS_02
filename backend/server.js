7b5bbfb69fe925c201b68454463b79a2bc48edbc
Devansh Tiwari <devanshtiwari817@gmail.com>
2026-02-06 07:35:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://future-fs-02-pe62.vercel.app", 
     credentials: true,             
   })
 );
